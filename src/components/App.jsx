8cde55accf2ec6cd27c58d1748b50e27fe9c85a4
Natalia Trehubenko <nataly.111.0479@gmail.com>
2026-09-12 14:27:37 +0300

    add config file

---
+++ src/components/App.jsx
@@ -0,0 +1,20 @@
+// import { useState } from 'react'
+
+import '../components/App.css'
+
+function App() {
+
+
+  return (
+    <>
+      <section id="center">
+        <div>
+          <h1>Travel Planner</h1>    
+          <p>Мои Путешествия</p>
+        </div>   
+      </section>
+    </>
+  )
+}
+
+export default App
