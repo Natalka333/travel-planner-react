9feb135607e412540b4810dbefc5754aa08c865c
Natalia Trehubenko <nataly.111.0479@gmail.com>
2026-09-12 18:54:02 +0300

    fix app

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,6 +2,16 @@
 
 import '../components/App.css'
 
+// const Product = ({ name, children }) => {
+//   return (
+//     <>
+//       <h2>All card</h2>
+//       <p>{name}{children}</p>
+//     </>
+//   )
+// }
+
+
 function App() {
 
 
@@ -11,6 +21,7 @@ function App() {
         <div>
           <h1>Travel Planner</h1>    
           <p>Мои Путешествия</p>
+          {/* <Product name="Spanish">MY TRAVEL</Product> */}
         </div>   
       </section>
     </>
